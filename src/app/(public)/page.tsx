75a64394f98255df105efdaf19819d85051fbb85
sato <ss.rin.3103@gmail.com>
2025-10-21 17:30:49 +0900

    58完了　記事一覧ページへの表示（変更ステージ漏れのため再コミット）

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,18 @@
-export default function PostsPage() {
+import { getPosts } from "@/lib/post"
+import PostCard from "@/components/post/PostCard"
+import { Post } from "@/types/post"
+
+export default async function PostsPage() {
+    const posts = await getPosts() as Post[] //まずはDBから記事の取得→使いやすいように定数に入れておく※asは型設定、複数の可能性で[]とする
     return(
-        <div>
-            
+        <>
+        <div className="container mx-auto px-4 py-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {posts.map((post)=>(
+                    <PostCard key={post.id} post={post} />
+                ))}
+            </div>
         </div>
+        </>
     )
 }
