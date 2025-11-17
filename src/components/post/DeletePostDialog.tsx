dffc3db099cd57bba00a6a35b2541f3217a4c287
sato <ss.rin.3103@gmail.com>
2025-11-17 17:25:33 +0900

    97完了　削除機能のアラートダイアログ追加

---
+++ src/components/post/DeletePostDialog.tsx
@@ -0,0 +1,37 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+
+type DeletePostProps = {
+    postId: string
+    isOpen: boolean
+    onOpenChange: (open: boolean) => void //void = 戻り値は空
+}
+export default function DeletePostDialog({postId,isOpen,onOpenChange}: DeletePostProps){
+    return(
+        <AlertDialog open={isOpen} onOpenChange={onOpenChange}> {/*ドロップダウンの情報を渡す*/}
+        <AlertDialogContent>
+            <AlertDialogHeader>
+            <AlertDialogTitle>記事の削除</AlertDialogTitle>
+            <AlertDialogDescription>
+                この記事を削除してもよろしいですか？
+                <br />
+                この操作は取り消せません。
+            </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+            <AlertDialogCancel>キャンセル</AlertDialogCancel>
+            <AlertDialogAction onClick={()=> deletePost(postId)}
+                className="bg-red-500 hover:bg-red-600">削除する</AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+        </AlertDialog>
+    )
+}
