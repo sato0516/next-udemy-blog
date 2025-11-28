4f7e95eb5bf948cd8bd32036865c18caf7cb24f7
sato <ss.rin.3103@gmail.com>
2025-11-28 18:01:46 +0900

    挙動修正②画像反映_postSchemaを変更

--- src/validations/post.ts
+++ src/validations/post.ts
@@ -8,5 +8,17 @@ export const postSchema = z.object({
     content:z.string()
     .min(10,{message:"内容は10文字以上で入力してください"}),
 
-    topImage:z.instanceof(File).nullable().optional()
+    //topImage:z.instanceof(File).nullable().optional()
+    topImage: z
+    .any()
+    .optional()
+    .refine(
+        (file) => {
+        if (!file) return true // 未設定はOK
+        // size プロパティを持っていて、5MB以内ならOK という判定
+        const size = (file as any).size
+        return typeof size === 'number' && size <= 5 * 1024 * 1024
+        },
+        { message: '画像は5MB以下のファイルを選択してください' }
+    ),
 })
